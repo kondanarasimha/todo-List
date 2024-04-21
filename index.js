051946a0ea751c6d8ba7ef7b81410e4b5629f8a7
Narasimha <kondanarasimha123@gmail.com>
2024-04-21 18:29:42 +0530

    Todo-List 1.0.0

--- index.js
+++ index.js
@@ -1,31 +1,43 @@
 const addButton = document.querySelector('.add-button');
 const todoInput = document.querySelector('.input-todo');
 const todoContainer = document.querySelector('.todos-container');
+const todoDateInput = document.querySelector('.todo-date');
 
-const todos = ['Drink water','Watch youtube'];
+
+const todos = [{
+  name:'Drink water',
+  date: '2024-04-21'
+  },
+  {
+    name:'Watch youtube',
+    date: '2024-04-21'
+  }];
 
   function renderTodos() {
     let todoHTML = '';
     todos.forEach((todo,i)=>{
       todoHTML += `
-      <p>${todo}</p>
+      <h3>${todo.name}</h3>
+      <h4>${todo.date}</h4>
       <button onclick='
         todos.splice(${i},${1});
         renderTodos();
       '>DELETE</button>`;
-    });//itraction
+    });
     todoContainer.innerHTML = todoHTML;
   }
+  renderTodos();
   
-
-
   addButton.addEventListener('click',()=> {
-    const todo = todoInput.value;
-    todos.push(todo);
-    console.log(todos);
+    const name = todoInput.value;
+    const date = todoDateInput.value;
+    todos.push({name,date});
     renderTodos();
     todoInput.value = '';
+    todoDateInput.value = '';
   });
+
+
 
 
 
