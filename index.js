e950f9e0ab64ad271ae661c5efca6d4890fc3020
Narasimha <kondanarasimha123@gmail.com>
2024-04-21 16:59:14 +0530

    Implemented delete functionality

--- index.js
+++ index.js
@@ -6,18 +6,25 @@ const todos = ['Drink water','Watch youtube'];
 
   function renderTodos() {
     let todoHTML = '';
-    todos.forEach((todo)=>{
-      todoHTML += `<h3>${todo}</h3>`;
+    todos.forEach((todo,i)=>{
+      todoHTML += `
+      <p>${todo}</p>
+      <button onclick='
+        todos.splice(${i},${1});
+        renderTodos();
+      '>DELETE</button>`;
     });//itraction
     todoContainer.innerHTML = todoHTML;
   }
   
 
-addButton.addEventListener('click',()=> {
+
+  addButton.addEventListener('click',()=> {
     const todo = todoInput.value;
     todos.push(todo);
     console.log(todos);
     renderTodos();
+    todoInput.value = '';
   });
 
 
